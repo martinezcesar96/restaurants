2316ac9372ff19bbbe20788e9abbc8ab969c5f12
César Martínez Serrano <netizenmx@gmail.com>
2024-05-03 13:39:08 -0600

    implements validator

--- src/main/models/restaurant.ts
+++ src/main/models/restaurant.ts
@@ -1,13 +1,46 @@
+import {
+  IsInt,
+  IsLatitude,
+  IsLongitude,
+  IsString,
+  IsUUID,
+  Max,
+  Min,
+} from 'class-validator';
+
 export class Restaurant {
+  @IsUUID()
   public id: string | null = null;
+
+  @IsInt()
+  @Max(4)
+  @Min(0)
   public rating: number | null = null;
+
+  @IsString()
   public name: string | null = null;
+
+  @IsString()
   public site: string | null = null;
+
+  @IsString()
   public email: string | null = null;
+
+  @IsString()
   public phone: string | null = null;
+
+  @IsString()
   public street: string | null = null;
+
+  @IsString()
   public city: string | null = null;
+
+  @IsString()
   public state: string | null = null;
+
+  @IsLatitude()
   public lat: number | null = null;
+
+  @IsLongitude()
   public lng: number | null = null;
 }
