336833b5a1fcddf66498456ab109da6c3cdf614e
César Martínez Serrano <netizenmx@gmail.com>
2024-05-04 11:10:32 -0600

    create database if not exits

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { APP_PORT } from './config/env/environment';
+import { setupDatabase } from './config/db/create-if-not-exist';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -19,5 +20,6 @@ async function bootstrap(): Promise<void> {
   SwaggerModule.setup('docs', app, document);
   await app.listen(APP_PORT);
 }
-
-bootstrap().catch(console.error);
+setupDatabase()
+  .then(() => bootstrap())
+  .catch(console.error);
